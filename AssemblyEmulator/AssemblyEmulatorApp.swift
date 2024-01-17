ff041a3e0bdb18bd1dee6a4ef78c48db27f37b92
maxland255 <69438306+maxland255@users.noreply.github.com>
2024-01-17 23:22:23 +0100

    Initial Commit

---
+++ AssemblyEmulator/AssemblyEmulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssemblyEmulatorApp.swift
+//  AssemblyEmulator
+//
+//  Created by Harry Pieteraerens on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AssemblyEmulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
