056c73f4e323d25601421903ff74657a687065e5
maxland255 <69438306+maxland255@users.noreply.github.com>
2024-01-18 22:28:56 +0100

    Add about window

---
+++ AssemblyEmulator/AboutWindow.swift
@@ -0,0 +1,18 @@
+//
+//  AboutWindow.swift
+//  AssemblyEmulator
+//
+//  Created by Harry Pieteraerens on 18/01/2024.
+//
+
+import SwiftUI
+
+struct AboutWindow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutWindow()
+}
