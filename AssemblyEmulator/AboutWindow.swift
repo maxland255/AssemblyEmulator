a401b3830a1c90593b40279089c3ff702cfcb3c4
maxland255 <69438306+maxland255@users.noreply.github.com>
2024-01-18 22:31:38 +0100

    Update about window

--- AssemblyEmulator/AboutWindow.swift
+++ AssemblyEmulator/AboutWindow.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct AboutWindow: View {
+    
+    @Environment(\.openURL) var openURL
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(nsImage: NSApp.applicationIconImage)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 125, height: 125)
+                .padding()
+            
+            Spacer()
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(NSApplication.appName)
+                    .font(.title.bold())
+                    .textSelection(.enabled)
+                        
+                Text("Version: " + NSApplication.appVersion + "   Build: " + NSApplication.buildVersion + "  " + NSApplication.beta)
+                    .font(.callout)
+                    .foregroundColor(.secondary)
+                                        
+                Text("AssemblyEmulator is an application designed to emulate assembly language. The primary goal is to simulate registers and RAM memory, utilizing an interpreter to execute assembly code and interact with the simulated registers and memory.")
+                    .font(.body)
+                        
+                Spacer()
+                        
+                HStack(spacing: 5) {
+                    Button {
+                        openURL(URL(string: "https://pieteraerens.eu")!)
+                    } label: {
+                        Text("WebSite")
+                    }
+                
+                    Spacer()
+                }
+            
+                Spacer()
+                
+                Text("Copyright © 2024 pieteraerens.eu")
+                    .font(.caption)
+                    .textSelection(.enabled)
+            }.padding()
+        
+            Spacer()
+        
+        }.frame(width: 500, height: 250)
     }
 }
 
--- AssemblyEmulator/AssemblyEmulatorApp.swift
+++ AssemblyEmulator/AssemblyEmulatorApp.swift
@@ -9,10 +9,93 @@ import SwiftUI
 
 @main
 struct AssemblyEmulatorApp: App {
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .frame(minWidth: 1000, idealWidth: 1000, maxWidth: .infinity, minHeight: 550, idealHeight: 550, maxHeight: .infinity)
         }.defaultSize(CGSize(width: 1000, height: 550))
+            .commands {
+                CommandGroup(replacing: .appInfo) {
+                    Button {
+                        appDelegate.toggleAboutWindow()
+                    } label: {
+                        Text("About \(NSApplication.appName)")
+                    }
+                }
+            }
+    }
+}
+
+
+class AppDelegate: NSObject, NSApplicationDelegate{
+        
+    private var aboutWindow: NSWindowController?
+    
+    func toggleAboutWindow(){
+        if self.aboutWindow == nil{
+            let styleMark: NSWindow.StyleMask = [.closable, .titled]
+            let window = NSWindow()
+            
+            window.styleMask = styleMark
+            window.title = "About \(NSApplication.appName)"
+            window.titlebarAppearsTransparent = true
+            window.center()
+            window.contentView = NSHostingView(rootView: AboutWindow())
+            window.backgroundColor = .controlBackgroundColor
+            
+            self.aboutWindow = NSWindowController(window: window)
+        }
+        
+        self.aboutWindow?.showWindow(self.aboutWindow?.window)
+    }
+}
+
+
+extension NSApplication{
+    static var appVersion: String{
+        return Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "Error"
+    }
+    
+    static var buildVersion: String{
+        return Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? "Error"
+    }
+    
+    static var appName: String{
+        return Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? "Error"
+    }
+    
+    static var minimumOSVersion: String{
+        return Bundle.main.object(forInfoDictionaryKey: "MinimumOSVersion") as? String ?? "Error"
+    }
+    
+    static var supportedPlatforms: Array<String>{
+        return Bundle.main.object(forInfoDictionaryKey: "CFBundleSupportedPlatforms") as? Array ?? ["Error"]
+    }
+    
+    static var localizations: Array<String>{
+        return Bundle.main.localizations
+    }
+    
+    static var developmentLocalization: String?{
+        return Bundle.main.developmentLocalization
+    }
+    
+    static var beta: String{
+        #if DEBUG
+        return "Development"
+        #else
+        if let url = Bundle.main.appStoreReceiptURL{
+            if url.lastPathComponent == "sandboxReceipt"{
+                return "Beta"
+            }else{
+                return "Release"
+            }
+        }else{
+            return "Error"
+        }
+        #endif
     }
 }
